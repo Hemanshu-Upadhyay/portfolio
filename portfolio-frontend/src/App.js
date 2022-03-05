50de007406143278b2c0745140c2f4fecfe51285
Hemanshu Upadhyay <hemanshu0412@Hemanshus-MacBook-Air.local>
2022-03-05 14:10:51 +0530

    Frontend Folder Structure initialized

---
+++ portfolio-frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {About, Footer, Header, Skills, Testimonial, Work} from './Container'
+
+function App() {
+  return (
+    
+    <div className="App">
+      <Header />
+      <About />
+      <Skills />
+      <Work />
+      <Testimonial />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ portfolio-frontend/src/Container/Testimonial/Testimonial.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './testimonial.scss'
+
+function Testimonial() {
+  return (
+    <div>Testimonial</div>
+  )
+}
+
+export default Testimonial
